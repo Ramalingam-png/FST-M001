975d2b590c1e0b06b89e173d6ed130df99f0be29
Ramalingam-png <ramalingambala643@gmail.com>
2024-12-20 19:29:11 +0530

    Add files via upload

---
+++ Java/Activities/Activity1.java
@@ -0,0 +1,17 @@
+package class_001;
+
+public class Activity1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car c = new Car();
+		c.color="Black";
+		c.transmission="manual";
+		c.make=2014;
+		c.displayCharacteristics();
+		c.accelarate();
+		c.brake();
+			
+	}
+
+}
---
+++ Java/Activities/Car.java
@@ -0,0 +1,31 @@
+package class_001;
+
+public class Car {
+
+	String color;
+	String transmission;
+	int make;
+	int tyres;
+	int doors;
+	
+	Car()
+	{
+		this.tyres=4;
+		this.doors=4;
+	}
+	public void displayCharacteristics() {
+	System.out.println(color);	
+	System.out.println(transmission);
+	System.out.println(make);
+	System.out.println(this.tyres);
+	System.out.println(this.doors);
+	}
+	public void accelarate() {
+		System.out.println("Car is moving forward");	
+	
+	}
+	public void brake() {
+		System.out.println("Car has stopped") ;
+		
+	}
+}
